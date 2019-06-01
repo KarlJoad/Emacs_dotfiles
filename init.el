19f93fee35ecce9610ca426255d24c11b4928321
Karl H <34152449+KarlJoad@users.noreply.github.com>
2019-06-01 13:55:29 -0500

    init.el: Made sure necessary packages are installed before starting emacs

--- init.el
+++ init.el
@@ -9,6 +9,15 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
+;;;; Ensure packages are always new and always loaded
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package-ensure) ; Make sure packages are loaded
+(setq use-package-always-ensure t)
+(use-package auto-compile
+	     :config (auto-compile-on-load-mode))
+
 ;;;; Change Default custom-theme-load-path
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
