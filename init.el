efba594375eea77dd10dd18fdbf5dda91ef8832c
KarlJoad <Khallsby@hawk.iit.edu>
2019-06-05 21:34:34 -0400

    init.el: Got LaTeX commands going
    Need to run each of the compilation commands with C-c C-c right now. Looking into how to compile whole document with single C-c C-a command.

--- init.el
+++ init.el
@@ -84,23 +84,52 @@
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (setq reftex-plug-into-AUCTeX t)
 
-
 ;;; preview-latex Options
 ;(load "preview-latex.el" nil t t) ; noerror-nil, nomessage-t, nosuffix-t
 
-;;;; Set the Theme
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files nil)
- '(package-selected-packages
-   (quote
-    (use-package spacemacs-theme neotree markdown-mode magit auto-compile auctex all-the-icons))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;; Set up the compilation options
+(function (lambda()
+	    '(TeX-command-list
+	      (quote
+	       (("TeX" "%(PDF)%(tex) %(file-line-error) %`%(extraopts) %S%(PDFout)%(mode)%' %t" TeX-run-TeX nil (plain-tex-mode texinfo-mode ams-tex-mode) :help "Run plain TeX")
+                 ("LaTeX" "%`%l%(mode)%' %T" TeX-run-TeX nil
+                 (latex-mode doctex-mode)
+		 :help "Run LaTeX")
+		 ("Makeinfo" "makeinfo %(extraopts) %t" TeX-run-compile nil
+		  (texinfo-mode)
+		  :help "Run Makeinfo with Info output")
+		 ("Makeinfo HTML" "makeinfo %(extraopts) --html %t" TeX-run-compile nil
+		  (texinfo-mode)
+		  :help "Run Makeinfo with HTML output")
+		 ("AmSTeX" "amstex %(PDFout) %`%(extraopts) %S%(mode)%' %t" TeX-run-TeX nil
+		  (ams-tex-mode)
+		  :help "Run AMSTeX")
+		 ("ConTeXt" "%(cntxcom) --once --texutil %(extraopts) %(execopts)%t" TeX-run-TeX nil
+		  (context-mode)
+		  :help "Run ConTeXt once")
+		 ("ConTeXt Full" "%(cntxcom) %(extraopts) %(execopts)%t" TeX-run-TeX nil
+		  (context-mode)
+		  :help "Run ConTeXt until completion")
+		 ("BibTeX" "bibtex %s" TeX-run-BibTeX nil t :help "Run BibTeX")
+		 ("Biber" "biber --input-directory ./TeX_Aux_Files --output-directory ./TeX_Aux_Files %s" TeX-run-Biber nil t :help "Run Biber with TeX_Aux_Files Directory")
+		 ("View" "%V" TeX-run-discard-or-function t t :help "Run Viewer")
+		 ("Print" "%p" TeX-run-command t t :help "Print the file")
+		 ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command)
+		 ("File" "%(o?)dvips %d -o %f " TeX-run-dvips t t :help "Generate PostScript file")
+		 ("Dvips" "%(o?)dvips %d -o %f " TeX-run-dvips nil t :help "Convert DVI file to PostScript")
+		 ("Dvipdfmx" "dvipdfmx %d" TeX-run-dvipdfmx nil t :help "Convert DVI file to PDF with dvipdfmx")
+		 ("Ps2pdf" "ps2pdf %f" TeX-run-ps2pdf nil t :help "Convert PostScript file to PDF")
+		 ("Glossaries" "makeglossaries %s" TeX-run-command nil t :help "Run makeglossaries to create glossary file")
+		 ("Index" "makeindex %s" TeX-run-index nil t :help "Run makeindex to create index file")
+		 ("upMendex" "upmendex %s" TeX-run-index t t :help "Run upmendex to create index file")
+		 ("Xindy" "texindy %s" TeX-run-command nil t :help "Run xindy to create index file")
+		 ("Check" "lacheck %s" TeX-run-compile nil
+		  (latex-mode)
+		  :help "Check LaTeX file for correctness")
+		 ("ChkTeX" "chktex -v6 %s" TeX-run-compile nil
+		  (latex-mode)
+		  :help "Check LaTeX file for common mistakes")
+		 ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document")
+		 ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files")
+		 ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files")
+		 ("Other" "" TeX-run-command t t :help "Run an arbitrary command"))))))
