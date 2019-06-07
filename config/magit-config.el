bc7a8cc305b2ae05bfbc23d9cd5ec485d4cf6261
KarlJoad <Khallsby@hawk.iit.edu>
2019-06-07 11:10:49 -0400

    Magit-config: "p" can now be used to git pull

--- config/magit-config.el
+++ config/magit-config.el
@@ -1,3 +1,8 @@
 ;;;; Provides my personal configurations for Magit
 (provide 'personal-magit)
+
+;;; Open Magit Status for git handling
 (global-set-key (kbd "C-x g") 'magit-status) ; C-x g will bring up magit status (git status)
+
+;;; git pull with now use p to pull
+(local-set-key (kbd "p") 'magit-pull) ; Using p in the magit major mode will pull that repo
