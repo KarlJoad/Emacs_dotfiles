225af2b1cd3d7a553e6abed2b774d0c6dcad5d8d
KarlJoad <Khallsby@hawk.iit.edu>
2019-06-09 19:41:16 -0400

    preview-latex: Added config options

--- config/preview-latex-config.el
+++ config/preview-latex-config.el
@@ -1,4 +1,10 @@
 ;;;; This file provides my personal changes to the preview-latex package
-(provide 'personal-preview-latex)
+(provide 'preview-latex-config)
 
 ;;; My settings go here
+
+(setq preview-image-type "png") ; Default Preview is png
+
+(setq preview-auto-cache-preamble t) ; Cache the preamble before running the first time, helpful for large preambles
+
+(setq preview-auto-reveal t) ; Automatically preview the results
