67227b36f919c03394f47e041fe8c8a419b2acbb
KarlJoad <Khallsby@hawk.iit.edu>
2019-06-26 16:18:40 -0500

    RefTeX: Started using reftex for LaTeX documents
    Need to configure this to do what I want. Refer to the manual I have
    pulled up.

--- config/reftex-config.el
+++ config/reftex-config.el
@@ -1,2 +1,8 @@
 ;;;; This is file provides my personal changes to the RefTeX package
 (provide 'reftex-config)
+
+;; Make sure that reftex gets pulled in when Emacs loads
+(require 'reftex)
+
+;; Make sure that reftex gets loaded when AucTeX gets loaded, when LaTeX file is opened
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
