7cf406f2a3d80ec51ecc6b407cf4522170c25312
KarlJoad <Khallsby@hawk.iit.edu>
2019-06-26 16:20:57 -0500

    Included company for latex math
    This may not be a required package, we'll see.
    We may not need company-auctex either.

--- config/package-config.el
+++ config/package-config.el
@@ -64,15 +64,23 @@
 ;; Make TAB act as if it were isussed in a vuffer of the language's major mode
 (setq org-src-tab-acts-natively t)
 
-;; Company gives us some auto-completion for things
+;; Company gives us an engine for some auto-completion for things we provide a backend for
 (use-package company
   :ensure t
   :defer t
   :init
   (global-company-mode)
   :config
-  (setq company-idle-delay 0.5)
+  (setq company-idle-delay 0)
   (define-key company-active-map (kbd "ESC") 'company-abort)
   (define-key company-active-map [tab] 'company-complete-common-or-cycle)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous))
+
+;; Company-AUCTeX provides the backend for Company
+;;(use-package company-auctex)
+;;(company-auctex-init)
+
+;; Company-math
+(use-package company-math)
+
