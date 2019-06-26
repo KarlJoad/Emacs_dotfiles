78f81aa6708da00093542985e5ec16ce7d25225b
KarlJoad <Khallsby@hawk.iit.edu>
2019-06-26 18:25:46 -0500

    Reftex: Made it play nice with AucTeX

--- config/reftex-config.el
+++ config/reftex-config.el
@@ -4,5 +4,8 @@
 ;; Make sure that reftex gets pulled in when Emacs loads
 (require 'reftex)
 
+;; Make RefTeX play nice with AucTeX
+(setq reftex-plug-into-AUCTeX t)
+
 ;; Make sure that reftex gets loaded when AucTeX gets loaded, when LaTeX file is opened
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
