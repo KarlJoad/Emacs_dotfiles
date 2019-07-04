9d75ef6c5b4afa0b95fb2579cf965ee644b56589
Karl H <34152449+KarlJoad@users.noreply.github.com>
2019-07-04 01:28:44 -0500

    Added the extra ; for Elisp comments

--- config/personal-settings.el
+++ config/personal-settings.el
@@ -5,26 +5,26 @@
 (scroll-bar-mode -1)
 
 ;;;; Skip the "Welcome" Page
-;(setq inhibit-startup-message t)
+;;(setq inhibit-startup-message t)
 
 ;;;; Turn on Line numbering
-(global-display-line-numbers-mode) ; Show line numbers everywhere
-(setq column-number-mode 1) ; Turn on column numbers in ALL major modes
-(global-hl-line-mode 1) ; Have line with my cursor highlighted
+(global-display-line-numbers-mode) ;; Show line numbers everywhere
+(setq column-number-mode 1) ;; Turn on column numbers in ALL major modes
+(global-hl-line-mode 1) ;; Have line with my cursor highlighted
 
-(show-paren-mode) ; Emphasize MATCHING Parentheses
-(setq blink-matching-paren nil) ; But don't let them blink
+(show-paren-mode) ;; Emphasize MATCHING Parentheses
+(setq blink-matching-paren nil) ;; But don't let them blink
 
-(setq auto-save-default t) ; Allow the #auto-save# files. They are removed upon buffer save anyways
-(setq make-backup-files nil) ; Disable backup~ files
+(setq auto-save-default t) ;; Allow the #auto-save# files. They are removed upon buffer save anyways
+(setq make-backup-files nil) ;; Disable backup~ files
 
-(setq visible-bell nil) ; Disable the visual bell
-(setq ring-bell-function 'ignore) ; Don't make a ding when failing command
+(setq visible-bell nil) ;; Disable the visual bell
+(setq ring-bell-function 'ignore) ;; Don't make a ding when failing command
 
 ;;; Can now open recently opened files
 (require 'recentf)
-(recentf-mode 1) ; Turn on recentf
-(setq recentf-max-saved-items 50) ; Maximum number of buffers to remember
+(recentf-mode 1) ;; Turn on recentf
+(setq recentf-max-saved-items 50) ;; Maximum number of buffers to remember
 
 ;;; Restore opened files from last session
 ;;(desktop-save-mode 1) ;; Commented out only while debugging my init files
