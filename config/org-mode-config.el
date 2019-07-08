c3d5759c1fa65d1e8786d20a61d31680db6a39c3
KarlJoad <Khallsby@hawk.iit.edu>
2019-07-08 17:08:47 -0500

    Org-Mode: All config changes in single config file

--- config/org-mode-config.el
+++ config/org-mode-config.el
@@ -5,3 +5,29 @@
 (global-set-key (kbd "C-c a") 'org-agenda) ; "C-c a" opens the Agenda Buffer to choose where to go
 (global-set-key (kbd "C-c l") 'org-store-link) ; "C-c l" stores a hyperlink to the cursor's current position in the current org-mode document
 ; (global-set-key (kbd "C-c c") 'org-capture) ; "C-c c" will let me select a template and file the new information
+
+;; Make org-babel work with source code blocks in Org docs
+(when (equal system-type 'windows-nt)
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((C . t)
+     (emacs-lisp . t)
+     (gnuplot . t)
+     (haskell . nil) ;; nil to not load haskell
+     (python . t)
+     (ruby . nil) ;; nil to not load ruby
+     (sql . t)
+     (sqlite . t))))
+
+(when (equal system-type 'gnu/linux)
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((C . t)
+     (emacs-lisp . t)
+     (gnuplot . t)
+     (haskell . nil) ;; nil to not load haskell
+     (python . t)
+     (ruby . nil) ;; nil to not load ruby
+     (sh . t)
+     (sql . t)
+     (sqlite . t))))
