647c3306d71d4861d8664e317bf60b866dc660f7
Karl H <34152449+KarlJoad@users.noreply.github.com>
2019-07-09 01:00:07 -0500

    Org-mode: Fixed Python indentation problem

--- config/org-mode-config.el
+++ config/org-mode-config.el
@@ -2,38 +2,41 @@
 (provide 'org-mode-config)
 
 ;;; Org-mode Keybindings (Agenda)
+;; These keybindings are set without needing an org file, because they should always be available.
 (global-set-key (kbd "C-c a") 'org-agenda) ; "C-c a" opens the Agenda Buffer to choose where to go
-(global-set-key (kbd "C-c l") 'org-store-link) ; "C-c l" stores a hyperlink to the cursor's current position in the current org-mode document
-; (global-set-key (kbd "C-c c") 'org-capture) ; "C-c c" will let me select a template and file the new information
+(global-set-key (kbd "C-c l") 'org-store-link) ; "C-c l" stores a hyperlink to the cursor's current position in the current Org-mode document
+;; (global-set-key (kbd "C-c c") 'org-capture) ; "C-c c" will let me select a template and file the new information
 
 ;; Make org-babel do stuff with  source code blocks in Org-mode
 ;; But only after there is an org file loaded up, otherwise, do nothing
+;; These are only loaded when an org file is first loaded because it is time-consuming to do this during Emacs startup
 (with-eval-after-load "org"
   (setq org-src-fontify-natively t) ;; Use syntax highliting in source blocks while editing
+  (setq org-src-preserve-indentation t) ;; DO NOT put the 2 leader spaces in source code. Prevents issues with white-space sensitive languages
   (setq org-src-tab-acts-natively t) ;; Make TAB act as if it were issued natively in that language's major mode
-  (setq org-src-window-setup 'current-window) ;; When C-c ' a code block, use same window
+  (setq org-src-window-setup 'current-window) ;; When C-c ' a code block, use same window as org file
   (setq org-confirm-babel-evaluate nil) ;; Don't ask before evaluating code blocks
-  (when (equal system-type 'windows-nt)
-    (org-babel-do-load-languages
+  ;; Host agnostic settings for Org-mode
+  (org-babel-do-load-languages
      'org-babel-load-languages
-     '((C . t)
-       (emacs-lisp . t)
-       (gnuplot . t)
+     '((C . t) ;; Works on C, C++, and D source blocks
+       (emacs-lisp . t) ;; For ELisp blocks
+       (java . t) ;; Java Blocks
+       (gnuplot . nil) ;; nil to not load gnuplot
        (haskell . nil) ;; nil to not load haskell
-       (python . t)
-       (ruby . nil) ;; nil to not load ruby
-       (sql . t)
-       (sqlite . t)))) ;; End of Windows Setup
+       (matlab . nil) ;; nil to not load MATLAB stuff
+       (octave . t) ;; Octave code blocks (Octave is a GNU replacement for MATLAB)
+       (python . t) ;; Python blocks
+       (R . t) ;; R blocks (Not sure when I'll use this, but I will some day)
+       (ruby . nil) ;; nil to not load Ruby
+       (sql . t) ;; SQL blocks
+       (sqlite . t) ;; SQLite blocks
+       ))
+  ;; Host OS-specific settings for Org-mode are below
+  ;; If Windows specific things must be put together, follow the gnu/linux example
   (when (equal system-type 'gnu/linux)
     (org-babel-do-load-languages
      'org-babel-load-languages
-     '((C . t)
-       (emacs-lisp . t)
-       (gnuplot . t)
-       (haskell . nil) ;; nil to not load haskell
-       (python . t)
-       (ruby . nil) ;; nil to not load ruby
-       (sh . t)
-       (sql . t)
-       (sqlite . t)))) ;; End of Linux Setup
+     '((sh . t)) ;; Bash Shell mode for editing bash files in Linux
+     )) ;; End of Linux Setup
   ) ;; End of Org-mode source code blocks setup
