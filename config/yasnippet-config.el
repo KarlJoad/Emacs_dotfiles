ebfeb9cfd142c13aeb1cb083bba5da8db518f5f7
Karl H <34152449+KarlJoad@users.noreply.github.com>
2019-07-28 20:24:49 -0500

    Yasnippet: Fixed list of extra snippets to load in

--- config/yasnippet-config.el
+++ config/yasnippet-config.el
@@ -14,7 +14,7 @@
   :defer t
   :config
   (yas-reload-all)
-  (setq yas-snippet-dirs '((concat user-emacs-directory "snippets"))) ;; Location to find MORE snippets
+  (setq yas-snippet-dirs (list (concat user-emacs-directory "snippets") yasnippet-snippets-dir)) ;; Location to find MORE snippets
   (setq tab-always-indent 'complete) ;; Tabs indent
   (setq yas-prompt-functions '(yas-completing-prompt ;; List of prompts that yasnippet can go through
 			       yas-ido-prompt
