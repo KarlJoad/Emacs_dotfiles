b26be4223d3e9514da08f8abd433ab8d4ea86456
KarlJoad <Khallsby@hawk.iit.edu>
2019-08-25 16:37:24 +0200

    Multiple Cursors: Bound functions to keyboard.
    C-S-c C-S-c will place a cursor on each line in selected region.
    Can also place cursors at locations based on keywords.

--- config/multiple-cursors-config.el
+++ config/multiple-cursors-config.el
@@ -4,5 +4,13 @@
 
 (use-package multiple-cursors)
 
+;; Place cursor on each line in selected region
+(global-set-key (kbd "C-S-c C-S-C") 'mc/edit-lines)
+
+;; Place cursors based on keywords in buffer
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
 (provide 'multiple-cursors-config)
 ;;; multiple-cursors-config ends here
