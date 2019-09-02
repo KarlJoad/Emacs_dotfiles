6ed6bd2e3693879006468106a4ecf60a4aacfc15
Karl Hallsby <khallsby@hawk.iit.edu>
2019-09-03 01:38:07 +0200

    Trying to get Java and Emacs to play well together

--- init.el
+++ init.el
@@ -58,8 +58,14 @@
 ;;;; Haskell Configs
 (require 'haskell-config)
 
+;;;; Java/Eclim Configs
+(require 'eclim-config)
+
 ;;;; Multiple Cursors
 (require 'multiple-cursors-config)
+
+;;;; Gradle Build System
+(require 'gradle-config)
 
 ;;;; flycheck for spell/syntax checking
 (require 'flycheck-config)
