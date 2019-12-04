2509d1712ff879e757fceec10e884e30791ae607
Karl Hallsby <khallsby@hawk.iit.edu>
2019-12-05 00:05:02 +0100

    Started working with web-mode
    Web-mode brings together several other major modes to allow us to edit
    web-side stuff in a single unified environment.

---
+++ config/web-mode-config.el
@@ -0,0 +1,10 @@
+;;; web-mode-config.el --- Provides and configures rainbow-mode
+;;; Commentary:
+;;; Code:
+
+(use-package web-mode
+  :ensure t
+  :defer t)
+
+(provide 'web-mode-config)
+;;; web-mode-config ends here
