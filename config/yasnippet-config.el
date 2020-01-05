4eaeec7051f692e22597868d30777434d412f77e
Karl Hallsby <khallsby@hawk.iit.edu>
2020-01-05 13:17:54 +0100

    Snippets: Instead of setting an unknown variable, define the variable

--- config/yasnippet-config.el
+++ config/yasnippet-config.el
@@ -14,7 +14,8 @@
   :defer t
   :config
   (yas-reload-all)
-  (setq yas-snippet-dirs (list (concat user-emacs-directory "snippets") yasnippet-snippets-dir)) ;; Location to find MORE snippets
+  (defvar yas-snippet-dirs (list (concat user-emacs-directory "snippets") yasnippet-snippets-dir) "List of directories to find snippets for the yasnippet package")
+  ;; (setq yas-snippet-dirs (list (concat user-emacs-directory "snippets") yasnippet-snippets-dir)) ;; Location to find MORE snippets
   (setq tab-always-indent 'complete) ;; Tabs indent
   (setq yas-prompt-functions '(yas-completing-prompt ;; List of prompts that yasnippet can go through
 			       yas-ido-prompt
