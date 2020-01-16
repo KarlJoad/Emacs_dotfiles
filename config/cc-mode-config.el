04e30ae3527914ea4c84335237d32793787529e9
Karl Hallsby <khallsby@hawk.iit.edu>
2020-01-16 11:33:19 +0100

    C Mode: C-c C-c will open compile function

--- config/cc-mode-config.el
+++ config/cc-mode-config.el
@@ -5,20 +5,24 @@
 
 (use-package ccls)
 
+
+(add-hook 'c-mode-hook
+	  (lambda ()
+	    (local-set-key (kbd "C-c C-c") 'compile)
+		(local-set-key (kbd "C-c ;") 'comment-region)))
+
 (add-hook 'c++-mode-hook
 	  (lambda ()
-	    (local-set-key (kbd "C-c C-c") 'compile)))
+	    (local-set-key (kbd "C-c C-c") 'compile)
+		(local-set-key (kbd "C-c ;") 'comment-region)))
 
 (setq-default c-default-style "stroustrup"
 	      c-basic-offset 4
 	      tab-width 4)
 
 ;; setup GDB
-(setq
- ;; use gdb-many-windows by default
- gdb-many-windows t
- ;; Non-nil means display source file containing the main routine at startup
- gdb-show-main t)
+(setq gdb-many-windows t ;; use gdb-many-windows by default
+	  gdb-show-main t) ;; Non-nil means display source file containing the main routine at startup
 
 (provide 'cc-mode-config)
 ;;; cc-mode-config.el ends here
