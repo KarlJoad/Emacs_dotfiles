ebefece87d3be37b845f7b340f816ae89f696413
Karl Hallsby <khallsby@hawk.iit.edu>
2020-02-07 13:55:27 +0100

    Rust: Went through and figured out some extra packages to add
    I actually skipped out on rust-mode, since it is not officially
    supported by GNU Emacs, and the Rust team doesn't keep it up-to-date.
    But, rustic is a fork of rust-mode that seems to behave much better.
    
    I have set up rustic to also work with the LSP, and plug into the
    rust-analyzer Cargo crate.
    
    I am also toying around with racer for code completion and source code
    navigation, though that one is a bit optional.

--- config/rust-config.el
+++ config/rust-config.el
@@ -8,5 +8,27 @@
 
 (use-package rustic) ;; Development environment for Rust
 
+(setq rustic-lsp-server 'rust-analyzer)
+(setq rustic-lsp-client 'lsp-mode)
+
+(add-hook 'rustic-mode-hook
+		  (lambda ()
+			(local-set-key (kbd "C-c <tab>") #'rustic-format-buffer)))
+
+;; (require company-mode)
+;; Racer is used for code completion and source code navigation
+(use-package racer)
+
+(setq racer-cmd "~/.cargo/bin/racer") ;; Binary path for rustup
+
+;; (add-hook 'rust-mode-hook #'racer-mode)
+;; (add-hook 'racer-mode-hook #'eldoc-mode)
+;; (add-hook 'racer-mode-hook #'company-mode)
+
+;; (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+
+;; Cargo is a package that allows us to control Rust's cargo from Emacs
+(use-package cargo)
+
 (provide 'rust-config)
 ;;; rust-config.el ends here
