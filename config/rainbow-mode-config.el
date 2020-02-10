212c4af7d75e6482bca3a11e3996fd10afa2d5f2
Karl Hallsby <khallsby@hawk.iit.edu>
2020-02-10 20:12:19 +0100

    Rainbow-mode: Added a commentary to ensure I know what rainbow-mode
    I get confused easily, and I was confusing rainbow-mode and
    rainbow-delimiters for quite some time.
    
    rainbow-mode colors phrases, hexadecimal values, etc. for what color
    they represent in the buffer.

--- config/rainbow-mode-config.el
+++ config/rainbow-mode-config.el
@@ -1,5 +1,9 @@
 ;;; rainbow-mode-config.el --- Provides and configures rainbow-mode
 ;;; Commentary:
+;;
+;; rainbow-mode is a package that colorizes the background of colors
+;; of things that are present in a buffer.
+;;
 ;;; Code:
 
 (use-package rainbow-mode
