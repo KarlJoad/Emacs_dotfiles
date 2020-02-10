785a5aaeddf79261550d77560116e716bf79a870
Karl Hallsby <khallsby@hawk.iit.edu>
2020-02-10 16:48:13 +0100

    Org: Made the eval-after-load a hook instead.

--- config/org-mode-config.el
+++ config/org-mode-config.el
@@ -26,31 +26,36 @@
 ;; Make org-babel do stuff with  source code blocks in Org-mode
 ;; But only after there is an org file loaded up, otherwise, do nothing
 ;; These are only loaded when an org file is first loaded because it is time-consuming to do this during Emacs startup
-(with-eval-after-load "org"
-  ;; Host agnostic settings for Org-mode
-  (org-babel-do-load-languages
-     'org-babel-load-languages
-     '((C . t) ;; Works on C, C++, and D source blocks
-       (emacs-lisp . t) ;; For ELisp blocks
-       (java . t) ;; Java Blocks
-       (gnuplot . nil) ;; nil to not load gnuplot
-       (haskell . nil) ;; nil to not load haskell
-       (matlab . nil) ;; nil to not load MATLAB stuff
-       (octave . t) ;; Octave code blocks (Octave is a GNU replacement for MATLAB)
-       (python . t) ;; Python blocks
-       (R . t) ;; R blocks (Not sure when I'll use this, but I will some day)
-       (ruby . nil) ;; nil to not load Ruby
-       (sql . t) ;; SQL blocks
-       (sqlite . t) ;; SQLite blocks
-       ))
-  ;; Host OS-specific settings for Org-mode are below
-  ;; If Windows specific things must be put together, follow the gnu/linux example
-  (when (equal system-type 'gnu/linux)
-    (org-babel-do-load-languages
-     'org-babel-load-languages
-     '((shell . t)) ;; Bash Shell mode for editing bash files in Linux
-     )) ;; End of Linux Setup
-  ) ;; End of Org-mode source code blocks setup
+;;(with-eval-after-load "org"
+(add-hook 'org-mode-hook
+		  (lambda ()
+			"Setup settings for Org-mode specifically"
+			;; Host agnostic settings for Org-mode
+			(org-babel-do-load-languages
+			 'org-babel-load-languages
+			 '((C . t) ;; Works on C, C++, and D source blocks
+			   (emacs-lisp . t) ;; For ELisp blocks
+			   (java . t) ;; Java Blocks
+			   (gnuplot . nil) ;; nil to not load gnuplot
+			   (haskell . nil) ;; nil to not load haskell
+			   (matlab . nil) ;; nil to not load MATLAB stuff
+			   (octave . t) ;; Octave code blocks (Octave is a GNU replacement for MATLAB)
+			   (python . t) ;; Python blocks
+			   (R . t) ;; R blocks (Not sure when I'll use this, but I will some day)
+			   (ruby . nil) ;; nil to not load Ruby
+			   (rust . t) ;; Rust
+			   (sml . t) ;; SML
+			   (sql . t) ;; SQL blocks
+			   (sqlite . t) ;; SQLite blocks
+			   ))
+			;; Host OS-specific settings for Org-mode are below
+			;; If Windows specific things must be put together, follow the gnu/linux example
+			(when (equal system-type 'gnu/linux)
+			  (org-babel-do-load-languages
+			   'org-babel-load-languages
+			   '((shell . t)) ;; Bash Shell mode for editing bash files in Linux
+			   )) ;; End of Linux Setup
+			)) ;; End of Org-mode source code blocks setup
 
 ;; This package minimizes bullets that are used in Org-mode
 (use-package org-bullets
