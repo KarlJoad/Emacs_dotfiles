c49eadd1d3d3d883a9d8721c573db765284873e2
Karl Hallsby <khallsby@hawk.iit.edu>
2020-02-10 19:59:52 +0100

    Personal: Use rainbow-delimiters in most programming languages
    This is done with the 'prog-mode-hook hook.
    However, if this doesn't cover all cases, I will have to manually
    specify the hook to run.

--- config/personal-settings.el
+++ config/personal-settings.el
@@ -23,10 +23,11 @@
   (setq column-number-mode 1) ;; Turn on column numbers in ALL major modes
   (global-hl-line-mode 1)) ;; Have line with my cursor highlighted
 
-
 (show-paren-mode) ;; Emphasize MATCHING Parentheses
 (setq blink-matching-paren nil) ;; But don't let them blink
 (use-package rainbow-delimiters) ;; Color nested parentheses/brackets/braces successively
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
 
 (setq auto-save-default t) ;; Allow the #auto-save# files. They are removed upon buffer save anyways
 (setq make-backup-files nil) ;; Disable backup~ files
