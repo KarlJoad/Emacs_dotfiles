9e762c16a3c4b6db9e1b3fe34ee9490cee098900
Karl Hallsby <khallsby@hawk.iit.edu>
2020-03-01 10:34:10 +0100

    Haskell: Ensured all packages were available and deferred until needed
    I also reorganized some of the code, so modes and their configurations
    are at the top. Checking and completion is at the bottom.

--- config/haskell-config.el
+++ config/haskell-config.el
@@ -2,7 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package haskell-mode)
+(use-package haskell-mode
+  :ensure t
+  :defer t)
 (add-hook 'haskell-mode-hook
 		  (lambda ()
 			(haskell-doc-mode) ;; Displays the type signature for a function
@@ -10,19 +12,31 @@
 			))
 
 ;; Sub-mode for Haskell-mode
-(use-package ghc)
+(use-package ghc
+  :ensure t
+  :defer t)
 
-;; flycheck stuff for Haskell
-(use-package flycheck-haskell)
 
-;; Yasnippet snippets for Haskell
-(use-package haskell-snippets)
 
 ;; Interface to Stack Haskell development tool
-(use-package hasky-stack)
+(use-package hasky-stack
+  :ensure t
+  :defer t)
 
 ;; Lookup Haskell documentation
-(use-package ghc-imported-from)
+(use-package ghc-imported-from
+  :ensure t
+  :defer t)
+
+;; flycheck stuff for Haskell
+(use-package flycheck-haskell
+  :ensure t
+  :defer t)
+
+;; Yasnippet snippets for Haskell
+(use-package haskell-snippets
+  :ensure t
+  :defer t)
 
 (provide 'haskell-config)
 ;;; haskell-config.el ends here
