0674169f8d347c9a640e6024a497d47a57596fa4
Karl Hallsby <khallsby@hawk.iit.edu>
2020-03-03 13:08:19 +0100

    Moved a lot of C/C++/Java related settings to the same file for ease

--- config/cc-mode-config.el
+++ config/cc-mode-config.el
@@ -5,20 +5,44 @@
 
 (use-package ccls)
 
+;; Available C style:
+;; “gnu”: The default style for GNU projects
+;; “k&r”: What Kernighan and Ritchie, the authors of C used in their book
+;; “bsd”: What BSD developers use, aka “Allman style” after Eric Allman.
+;; “whitesmith”: Popularized by the examples that came with Whitesmiths C, an early commercial C compiler.
+;; “stroustrup”: What Stroustrup, the author of C++ used in his book
+;; “ellemtel”: Popular C++ coding standards as defined by “Programming in C++, Rules and Recommendations,” Erik Nyquist and Mats Henricson, Ellemtel
+;; “linux”: What the Linux developers use for kernel development
+;; “python”: What Python developers use for extension modules
+;; “java”: The default style for java-mode (see below)
+;; “user”: When you want to define your own style
+(setq c-default-style
+	  '((java-mode . "java")
+		(other . "linux")))
+
+(use-package company-c-headers
+  :init
+  (add-to-list 'company-backends 'company-c-headers))
+
+(use-package company-ctags
+  :ensure t
+  :defer t)
+
+(use-package company-lsp)
+
+(setq c-default-style "linux") ;; set style to "linux"
 
 (add-hook 'c-mode-hook
 	  (lambda ()
 	    (local-set-key (kbd "C-c C-c") 'compile)
+		(local-set-key (kbd "C-c SPC") 'company-complete)
 		(local-set-key (kbd "C-c ;") 'comment-region)))
 
 (add-hook 'c++-mode-hook
 	  (lambda ()
 	    (local-set-key (kbd "C-c C-c") 'compile)
+		(local-set-key (kbd "C-c SPC") 'company-complete)
 		(local-set-key (kbd "C-c ;") 'comment-region)))
-
-(setq-default c-default-style "stroustrup"
-	      c-basic-offset 4
-	      tab-width 4)
 
 ;; setup GDB
 (setq gdb-many-windows t ;; use gdb-many-windows by default
