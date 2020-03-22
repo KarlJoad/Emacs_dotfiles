1d0ecd444b0192647c7a1455f37a6f570b5863ae
Karl Hallsby <khallsby@hawk.iit.edu>
2020-03-22 11:58:05 +0100

    Docker: Added configuration to bring Docker support into Emacs
    I am going to have to use Docker for my Operating Systems class, so I
    figured I would bring that stuff into Emacs, to make my time a little
    bit easier.

---
+++ config/docker-config.el
@@ -0,0 +1,32 @@
+;;; docker-config.el --- This file sets up packages for workgin with Docker in Emacs
+;;; Commentary:
+;;; Code:
+
+;; Emacs Interface to Docker
+(use-package docker
+  :ensure t
+  :defer t
+  :bind ("C-c d" . 'docker))
+
+;; Emacs Interface to the Docker API
+(use-package docker-api
+  :ensure t
+  :defer t)
+
+;; For running various commands in Docker containers
+(use-package docker-cli
+  :ensure t
+  :defer t)
+
+;; Major mode for editing docker-compose files
+(use-package docker-compose-mode
+  :ensure t
+  :defer t)
+
+;; Major mode for editing Docker's Dockerfiles
+(use-package dockerfile-mode
+  :ensure t
+  :defer t)
+
+(provide 'docker-config)
+;;; docker-config.el ends here
