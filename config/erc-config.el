79433ede27d093521083a52dc3efff1e37569460
Karl Hallsby <karl@hallsby.com>
2020-06-28 00:24:27 +0200

    Add erc configurations

--- config/erc-config.el
+++ config/erc-config.el
@@ -3,6 +3,13 @@
 ;; ERC is the Emacs major mode for IRC conversations and channels.
 ;;; Code:
 
+(require 'erc)
+
+(erc-match-mode 1)
+(add-to-list 'erc-keywords "\\KarlJoad\\b")
+(add-to-list 'erc-keywords "\\KarlJoad`\\b")
+
+(setq  erc-server-coding-system '(utf-8 . utf-8))
 
 
 (provide 'erc-config)
