b8639f24858cfb6c06b00e904e6c8662ee37b8e3
Karl Hallsby <karl@hallsby.com>
2020-11-17 11:42:39 -0600

    Verilog: Add basic/standard user-settable variables

--- config/verilog-config.el
+++ config/verilog-config.el
@@ -7,5 +7,21 @@
 (autoload 'verilog-mode "verilog-mode" "Verilog mode" t)
 (add-to-list 'auto-mode-alist '("\\.[ds]?va?h?\\'" . verilog-mode))
 
+(setq verilog-indent-level             3
+      verilog-indent-level-module      3
+      verilog-indent-level-declaration 3
+      verilog-indent-level-behavioral  3
+      verilog-indent-level-directive   1
+      verilog-case-indent              2
+      verilog-auto-newline             t
+      verilog-auto-indent-on-newline   t
+      verilog-tab-always-indent        t
+      verilog-auto-endcomments         t
+      verilog-minimum-comment-distance 40
+      verilog-indent-begin-after-if    t
+      verilog-auto-lineup              'declarations
+      verilog-linter                   "my_lint_shell_command"
+      )
+
 (provide 'verilog-config)
 ;;; verilog-config.el ends here
