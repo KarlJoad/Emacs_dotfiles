cdb04a0f8e84f96d54956bcf1aa48c4d27bb1fa1
Karl Hallsby <karl@hallsby.com>
2021-01-21 17:29:57 -0600

    Further ensure UTF-8 is used everywhere possible

--- config/personal-settings.el
+++ config/personal-settings.el
@@ -76,7 +76,9 @@
 (global-set-key (kbd "C-c g") 'revert-buffer)
 
 ;; Try to use UTF-8 for everything
+(set-language-environment "UTF-8")
 (setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
