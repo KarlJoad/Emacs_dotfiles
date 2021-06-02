54e926f8e4135b4bb6c68973914cf931a3d2a758
Karl Hallsby <karl@hallsby.com>
2021-06-02 18:13:44 -0500

    Setup some ERC information for IRC communications

--- config/erc-config.el
+++ config/erc-config.el
@@ -11,6 +11,13 @@
 
 (setq  erc-server-coding-system '(utf-8 . utf-8))
 
+(setq erc-server "irc.libera.chat"
+	  erc-nick "KarlJoad"
+	  erc-user-full-name "Karl"
+	  erc-track-shorten-start 8 ; Show first 8 characters of channel name with activity
+	  erc-autojoin-channels-alist '(("irc.libera.chat" "#nixos"))
+	  erc-kill-buffer-on-part t) ; Kill a channel's buffer when I part from it.
+
 
 (provide 'erc-config)
 ;;; erc-config.el ends here
