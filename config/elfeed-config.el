481e746f22c6c24ba88558cfcbcf64ef163f7faf
Karl Hallsby <karl@hallsby.com>
2021-10-07 00:53:31 -0500

    Add additional RSS feeds for elfeed

--- config/elfeed-config.el
+++ config/elfeed-config.el
@@ -10,7 +10,10 @@
 ;; List of all feeds that I should fetch and care about
 ;; The cdr (tail of list) will be symbols attached to anything coming from that
 ;; particular feed.
-(setq elfeed-feeds '(("https://karl.hallsby.com/feed.xml" personal blog)))
+(setq elfeed-feeds '(("https://karl.hallsby.com/feed.xml" personal blog)
+                     ("https://xkcd.com/atom.xml" comic webcomic)
+                     ("https://guix.gnu.org/feeds/blog.atom" guix reproducible)
+                     ("https://protesilaos.com/codelog.xml" emacs blog)))
 
 ;; Prettify the elfeed buffer, making some things easier to read
 (use-package elfeed-goodies)
