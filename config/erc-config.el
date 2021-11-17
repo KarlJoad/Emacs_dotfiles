b3de6eca2dfb9c748310ec51f08faea429cd2537
Karl Hallsby <karl@hallsby.com>
2021-11-17 17:26:47 -0600

    Use ZNC server for IRC config on DESKTOP

--- config/erc-config.el
+++ config/erc-config.el
@@ -10,17 +10,28 @@
 (add-to-list 'erc-keywords "\\KarlJoad`\\b")
 (add-to-list 'erc-keywords "\\KarlJoad-\\b")
 
-(setq erc-server "irc.libera.chat"
-	  erc-nick "KarlJoad"
-	  erc-user-full-name "Karl"
-	  erc-track-shorten-start 8 ; Show first 8 characters of channel name with activity
-	  erc-autojoin-channels-alist '(("irc.libera.chat" "#nixos"))
-	  erc-kill-buffer-on-part t ; Kill a channel's buffer when I part from it.
-    erc-modules
-    '(button completion fill irccontrols match menu readonly ring spelling stamp
-             notifications))
 (setq erc-server-coding-system '(utf-8 . utf-8))
 
+;; (setq erc-server "irc.libera.chat"
+;; 	    erc-nick "KarlJoad"
+;; 	    erc-user-full-name "Karl"
+;; 	    erc-track-shorten-start 8 ; Show first 8 characters of channel name with activity
+;; 	    erc-autojoin-channels-alist '(("irc.libera.chat" "#nixos"))
+;; 	    erc-kill-buffer-on-part t ; Kill a channel's buffer when I part from it.
+;;       erc-modules
+;;       '(button completion fill irccontrols match menu readonly ring spelling stamp
+;;                notifications))
+
+(setq erc-server "192.168.1.2"
+      erc-port 5000
+	    erc-nick "KarlJoad"
+	    erc-user-full-name "Karl"
+	    erc-track-shorten-start 8 ; Show first 8 characters of channel name with activity
+	    erc-autojoin-channels-alist '(("irc.libera.chat" "#nixos"))
+	    erc-kill-buffer-on-part t ; Kill a channel's buffer when I part from it.
+      erc-modules
+      '(button completion fill irccontrols match menu readonly ring spelling stamp
+               notifications))
 
 (use-package erc-hl-nicks
   :after erc
