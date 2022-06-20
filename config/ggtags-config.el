a4a5b75520963017044f95cf84953ad99e2db0e6
Karl Hallsby <karl@hallsby.com>
2022-06-19 20:26:19 -0500

    Correct indentation of ggtags keybindings

--- config/ggtags-config.el
+++ config/ggtags-config.el
@@ -9,21 +9,15 @@
   :defer t
   :bind
   (:map global-map
-	("C-x t c" . 'ggtags-create-tags)
-	("C-x t u" . 'ggtags-update-tags)
-	("C-x t f" . 'ggtags-find-file)
-	("C-x t r" . 'ggtags-find-reference)
-	("C-x t d" . 'ggtags-find-definition)
-	("C-x t h" . 'ggtags-view-tag-history)
-	("C-x t s" . 'ggtags-find-other-symbol)))
+	      ("C-x t c" . 'ggtags-create-tags)
+	      ("C-x t u" . 'ggtags-update-tags)
+	      ("C-x t f" . 'ggtags-find-file)
+	      ("C-x t r" . 'ggtags-find-reference)
+	      ("C-x t d" . 'ggtags-find-definition)
+	      ("C-x t h" . 'ggtags-view-tag-history)
+	      ("C-x t s" . 'ggtags-find-other-symbol)))
 
 (add-hook 'c-mode-common-hook
-	  (lambda ()
-	    (when (derived-mode-p 'c-mode
-				  'c++-mode
-				  'java-mode
-				  'asm-mode
-				  'python-mode))))
 
 (ggtags-mode 1) ;; Turn ggtags on in every buffer
 
