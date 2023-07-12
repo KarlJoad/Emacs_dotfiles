00eaf6e0b93b54dcbb97e692cfb424c3bc3658aa
Karl Hallsby <karl@hallsby.com>
2023-07-11 23:37:07 -0500

    Auto-join the spritely IRC room

--- config/erc-config.el
+++ config/erc-config.el
@@ -17,7 +17,8 @@
 	    erc-nick "RavenJoad"
 	    erc-user-full-name "Raven"
 	    erc-track-shorten-start 8 ; Show first 8 characters of channel name with activity
-	    erc-autojoin-channels-alist '(("irc.libera.chat" "#guix" "#guile" "#nixos"))
+	    erc-autojoin-channels-alist '(("irc.libera.chat" "#guix" "#guile" "#spritely"
+                                     "#nixos"))
 	    erc-kill-buffer-on-part t ; Kill a channel's buffer when I part from it.
       erc-modules
       '(button completion fill irccontrols match menu readonly ring spelling stamp
