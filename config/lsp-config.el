40c7573ce989321f163a3ed4bbe43982dd440e98
Karl Hallsby <karl@hallsby.com>
2023-12-04 20:40:21 -0600

    Exchange lsp-mode for eglot
    eglot is built into emacs after Emacs 29, and will continue to be
    supported throughout Emacs' life. Plus, it integrates with things
    already built into Emacs, such as:
      * eldoc for documentation
      * xref for definition/reference lookups
      * yasnippet for snippet completion
      * flymake/flycheck for showing errors
      * and more!
    
    By using this, I remove heavy-handed dependencies on packages outside
    of Emacs, which I hope should reduce the overall footprint of Emacs
    when using an LSP.

--- config/lsp-config.el
+++ config/lsp-config.el
@@ -2,78 +2,31 @@
 ;;; Commentary:
 ;;; Code:
 
-
-(use-package lsp-mode
+(use-package eglot
   :straight t
-  :defer t
   :ensure t
-  :hook ((lsp-mode . lsp-diagnostics-mode)
-         (lsp-mode . (lambda () (lsp-completion-mode 1)))
-         (c-ts-mode . lsp-deferred)
-         (c++-ts-mode . lsp-deferred))
-  :init
-  (setq lsp-use-plists t)
+  :defer t
+  :hook (((c-mode c++-mode c-ts-mode c++-ts-mode) . eglot-ensure))
   :custom
-  (lsp-keymap-prefix "C-c l")
-  (lsp-keep-workspace-alive nil)
-  (lsp-idle-delay 0.5)
-  ;; core
-  (lsp-enable-xref t)
-  ;; Disable LSP from configuring everything itself. I must configure LSP manually.
-  (lsp-auto-configure nil)
-  (lsp-eldoc-enable-hover nil)
-  (lsp-enable-dap-auto-configure nil)
-  (lsp-enable-file-watchers nil)
-  (lsp-enable-folding nil)
-  (lsp-enable-imenu nil)
-  ;; LSP should not ask me to download LSP servers.
-  (lsp-enable-suggest-server-download nil)
-  ;; Prevent LSP from providing highlighting, since we use tree-sitter-based or
-  ;; Emacs' built-in regexp-based major modes.
-  (lsp-enable-indentation nil)
-  (lsp-enable-links nil)
-  (lsp-enable-on-type-formatting nil)
-  (lsp-enable-symbol-highlighting nil)
-  (lsp-enable-text-document-color nil)
-  ;; completion
-  (lsp-completion-enable t)
-  (lsp-completion-enable-additional-text-edit nil)
-  (lsp-enable-snippet nil)
-  (lsp-completion-show-kind nil)
-  ;; headerline
-  (lsp-headerline-breadcrumb-enable nil)
-  (lsp-headerline-breadcrumb-enable-diagnostics nil)
-  (lsp-headerline-breadcrumb-enable-symbol-numbers nil)
-  (lsp-headerline-breadcrumb-icons-enable nil)
-  ;; modeline
-  (lsp-modeline-code-actions-enable nil)
-  (lsp-modeline-diagnostics-enable nil)
-  (lsp-modeline-workspace-status-enable nil)
-  (lsp-signature-doc-lines 1)
-  ;; lens
-  (lsp-lens-enable nil)
-  ;; semantic
-  (lsp-semantic-tokens-enable nil))
+  (eglot-autoshutdown t)
+  ;; For performance, set this to a low number. When debugging, comment this out.
+  ;; Setting to 0 means no messages/events are logged in the EGLOT events buffer.
+  (eglot-events-buffer-size 0)
+  ;; XRef look-ups can leave the project Eglot is running a server for
+  (eglot-extend-to-xref t)
+  (eglot-ignored-server-capabilities
+   '(;; Disable LSP from providing highlighting, since I use treesitter-based or
+     ;; Emacs' built-in regexp-based major modes for font-locking.
+     :colorProvider
+     :documentHighlightProvider
+     :documentFormattingProvider
+     :documentRangeFormattingProvider
+     :documentOnTypeFormattingProvider
+     :foldingRangeProvider))
+  (eglot-stay-out-of '(yasnippet)))
 
-;; Register clangd as an LSP backend for c-ts-mode & c++-ts-mode
-(with-eval-after-load 'lsp-mode
-  (lsp-register-client
-   (make-lsp-client :new-connection (lsp-stdio-connection "clangd")
-                    :activation-fn (lsp-activate-on "c")
-                    :server-id 'clangd))
-  (lsp-register-client
-   (make-lsp-client :new-connection (lsp-stdio-connection "clangd")
-                    :activation-fn (lsp-activate-on "cpp")
-                    :server-id 'clangd)))
-
-;; Fancy sideline, Popup documentation, VS Code-like peek UI
-(use-package lsp-ui
-  :straight t
-  :ensure t
-  :defer t
-  :commands lsp-ui-mode)
-
-(add-hook 'lsp-mode-hook #'lsp-ui-mode)
+;; (or (getenv "GUIX_ENVIRONMENT")
+;;     (getenv "IN_NIX_SHELL"))
 
 (provide 'lsp-config)
 ;;; lsp-config.el ends here
