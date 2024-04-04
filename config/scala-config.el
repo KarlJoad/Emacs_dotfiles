248c28ca1f889146b7e1862d53bb30f5d9a333d2
Karl Hallsby <karl@hallsby.com>
2024-04-04 17:13:08 -0500

    Disable lsp-metals
    lsp-metals currently requires scala-mode 1.1, which is not available
    anywhere. I do not use an LSP for Scala/Chisel anyways, so it is not
    terribly important for me.

--- config/scala-config.el
+++ config/scala-config.el
@@ -14,16 +14,16 @@
   :defer t)
 
 ;; LSP mode for handling scala with LSP
-(use-package lsp-metals
-  :ensure t
-  :defer t
-  :after (eglot scala-mode)
-  :custom
-  ;; Metals claims to support range formatting by default but it supports range
-  ;; formatting of multiline strings only. You might want to disable it so that
-  ;; emacs can use indentation provided by scala-mode.
-  (lsp-metals-server-args '("-J-Dmetals.allow-multiline-string-formatting=off"))
-  :hook (scala-mode . eglot-ensure))
+;; (use-package lsp-metals
+;;   :ensure t
+;;   :defer t
+;;   :after (eglot scala-mode)
+;;   :custom
+;;   ;; Metals claims to support range formatting by default but it supports range
+;;   ;; formatting of multiline strings only. You might want to disable it so that
+;;   ;; emacs can use indentation provided by scala-mode.
+;;   (lsp-metals-server-args '("-J-Dmetals.allow-multiline-string-formatting=off"))
+;;   :hook (scala-mode . eglot-ensure))
 
 (provide 'scala-config)
 ;;; scala-config.el ends here
