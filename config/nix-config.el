6d6621423c63006bff0678d4f68eb54b263f9c49
Karl Hallsby <karl@hallsby.com>
2024-04-05 11:16:41 -0500

    Ensure nix packages are installed by elpaca

--- config/nix-config.el
+++ config/nix-config.el
@@ -11,14 +11,19 @@
 ;; Each of these is deferred because I won't necessarily edit Nix files on every GNU/Linux
 ;; computer that I use
 (use-package nix-mode ;; Major mode for editing *.nix files
+  :ensure t
   :defer t)
 (use-package json-mode ;; nix-mode needs json-mode for some reason
+  :ensure t
   :defer t)
 (use-package nix-buffer
+  :ensure t
   :defer t)
 (use-package nix-update
+  :ensure t
   :defer t)
 (use-package nixos-options ;; Options for the Nixos .nix files
+  :ensure t
   :defer t)
 
 (provide 'nix-config)
