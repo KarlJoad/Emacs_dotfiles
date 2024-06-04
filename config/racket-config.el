42f20a7bf4847633fe8dce0deba2be19591d95a0
Karl Hallsby <karl@hallsby.com>
2024-06-04 12:00:58 -0500

    Use upstream racket-mode, rather than custom version
    I wanted to have Guix and direnv manage my Racket installations on a
    per-project basis. In particular, I wanted to be able to have multiple
    versions on Racket on my system at one time. However, Racket is not
    architected in a way that makes that easy. Instead, Racket uses a
    single back-end binary which then provides multiple REPLs that
    interact with it.
    
    After discussion with racket-mode's author, we together came to the
    conclusion that hooking envrc to set racket-mode's back-end would only
    cause problems. As documented in the GitHub issue thread [1], and in
    the documentation, the way to do this on a per-project basis is to
    have it handled by .dir-locals.el using the following snippet:
    
      ((nil . ((eval . (racket-add-back-end default-directory)))))
    
    [1] https://github.com/greghendershott/racket-mode/issues/706

--- config/racket-config.el
+++ config/racket-config.el
@@ -1,19 +1,13 @@
 ;;; racket-config.el --- Configure Racket for Emacs -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-;; (use-package el-patch
-;;   :ensure (el-patch :type git :host github :repo "radian-software/el-patch"
-;;                       :fork (:host github
-;;                              :repo "your-name/el-patch")))
 
 (require 'magit)
 (require 'lispy-config)
 (require 'yasnippet-config)
 
 (use-package racket-mode
-  :ensure `(racket-mode :type git :host github :repo "greghendershott/racket-mode"
-                        :fork (:host github
-                                     :repo "KarlJoad/racket-mode"))
+  :ensure t
   :hook ((racket-mode . racket-xp-mode)
          (racket-mode . racket-unicode-input-method-enable)
          (racket-repl-mode . racket-unicode-input-method-enable)
