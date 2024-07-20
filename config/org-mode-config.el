344b4c3b32557ab0e29a77d0fb83ca78a9d7406d
Karl Hallsby <karl@hallsby.com>
2024-07-19 22:30:30 -0500

    Fix indentation on Org-mode key-binding alist

--- config/org-mode-config.el
+++ config/org-mode-config.el
@@ -9,10 +9,10 @@
   :ensure nil ;; built-in
   :defer t
   :bind (;; These keybindings are set without needing an org file, because they should always be available.
-	 ("C-c a" . 'org-agenda) ;; "C-c a" opens the Agenda Buffer to choose where to go
-	 ("C-c l" . 'org-store-link) ;; "C-c l" stores a hyperlink to the cursor's current position in the current Org-mode document
-	 ("C-c c" . 'org-capture) ;; "C-c c" will let me select a template and file the new information
-	 )
+	       ("C-c a" . 'org-agenda) ;; "C-c a" opens the Agenda Buffer to choose where to go
+	       ("C-c l" . 'org-store-link) ;; "C-c l" stores a hyperlink to the cursor's current position in the current Org-mode document
+	       ("C-c c" . 'org-capture) ;; "C-c c" will let me select a template and file the new information
+	       )
   :config
   ;; Only load org-babel languages after loading org-mode, which we do lazily.
   ;; This is done lazily because it is time-consuming to do this during startup.
