0cf0b30c8533bf60864ead60b4d0ddfb2fb4aa65
Karl Hallsby <karl@hallsby.com>
2024-09-01 15:57:48 -0500

    Make magit-todos ignore things I do not control in my emacs config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,3 @@
-((emacs-lisp-mode . ((indent-tabs-mode . nil)
+((nil . ((eval . (setq-local magit-todos-exclude-globs (append (list "snippets/" "themes/") magit-todos-exclude-globs)))))
+ (emacs-lisp-mode . ((indent-tabs-mode . nil)
                      (fill-column . 80))))
