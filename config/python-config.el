940e4eb6e4ecb9d61bd31abef4b3c8da189b188e
Karl Hallsby <karl@hallsby.com>
2024-11-05 10:31:41 -0600

    When using Python major-modes, ignore __pycache__ from completions
    find-file and co. use Emacs's completion system, which I have
    augmented with a handful of packages. However, I don't want to see the
    __pycache__/ directory as a potential completion, because the only
    thing it contains is JIT-compiled bytecode for Python. There will
    almost never be a time where I want to look inside that directory, so
    we ask the completion system to ignore it by default.
    
    If I REALLY wanted to look inside __pycache__/, I can still get to it
    by name. For example, the sequence "C-x C-f __py" will cause Emacs'
    completion system to FORCE completing __pycache__/.
    
    Note that this override will affect ANYTHING that uses the completion
    system, this includes project-find-file, and anything else.

--- config/python-config.el
+++ config/python-config.el
@@ -5,12 +5,16 @@
 (use-package python-mode
   :ensure nil ; built-in
   :defer t
+  :config
+  (add-to-list 'completion-ignored-extensions "__pycache__/")
   :custom
   (python-indent-offset 4))
 
 (use-package python-ts-mode
   :ensure nil ; built-in
   :defer t
+  :config
+  (add-to-list 'completion-ignored-extensions "__pycache__/")
   :custom
   (python-indent-offset 4))
 
