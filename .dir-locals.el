946884a07cfe4bb8ce2269edf0491ebf9d9a960a
Karl Hallsby <karl@hallsby.com>
2025-02-23 19:16:07 -0600

    Wrap .dir-locals.el to 80 characters

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,5 @@
-((nil . ((eval . (setq-local magit-todos-exclude-globs (append (list "snippets/" "themes/") magit-todos-exclude-globs)))))
+((nil . ((eval . (setq-local magit-todos-exclude-globs
+                             (append (list "snippets/" "themes/")
+                                     magit-todos-exclude-globs)))))
  (emacs-lisp-mode . ((indent-tabs-mode . nil)
                      (fill-column . 80))))
