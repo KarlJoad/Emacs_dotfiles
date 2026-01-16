84e23f3992cd4357e1cb3d5371507cd01306fb41
Karl Hallsby <karl@hallsby.com>
2026-01-15 23:40:18 -0600

    Replace yasnippet with tempel

--- config/snippets-config.el
+++ config/snippets-config.el
@@ -15,34 +15,46 @@
 ;;
 ;;; Code:
 
-(use-package yasnippet
+(use-package tempel
   :ensure t
   :defer t
-  :bind (:map yas-minor-mode-map
-         ;; If there was a menu for selecting snippets, provide an escape route
-         ("<escape>" . #'yas-exit-snippet))
-  :config
-  (yas-reload-all)
-  (yas-global-mode 1)
-  (defvar yas-snippet-dirs
-    (list (concat user-emacs-directory "snippets") yasnippet-snippets-dir)
-    "List of directories to find snippets for the yasnippet package.")
-  :custom
-  (tab-always-indent 'complete) ;; Tabs indent
-  (yas-prompt-functions
-   '(yas-completing-prompt ;; List of prompts that yasnippet can go through
-     yas-ido-prompt
-     yas-dropdown-prompt)))
+  :bind (("M-+" . tempel-complete) ;; Alternative tempel-expand
+         ("M-*" . tempel-insert))
+
+  :init
+  ;; Setup completion at point
+  (defun tempel-setup-capf ()
+    ;; Add the Tempel Capf to `completion-at-point-functions'.  `tempel-expand'
+    ;; only triggers on exact matches. We add `tempel-expand' *before* the main
+    ;; programming mode Capf, such that it will be tried first.
+    (setq-local completion-at-point-functions
+                (cons #'tempel-expand completion-at-point-functions))
+
+    ;; Alternatively use `tempel-complete' if you want to see all matches.  Use
+    ;; a trigger prefix character in order to prevent Tempel from triggering
+    ;; unexpectly.
+    ;; (setq-local corfu-auto-trigger "/"
+    ;;             completion-at-point-functions
+    ;;             (cons (cape-capf-trigger #'tempel-complete ?/)
+    ;;                   completion-at-point-functions))
+    )
+
+  ;; (add-hook 'conf-mode-hook #'tempel-setup-capf)
+  (add-hook 'prog-mode-hook #'tempel-setup-capf)
+  ;; (add-hook 'text-mode-hook #'tempel-setup-capf)
+
+  ;; Optionally make the Tempel templates available to Abbrev,
+  ;; either locally or globally. `expand-abbrev' is bound to C-x '.
+  ;; (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+  ;; (global-tempel-abbrev-mode)
+  )
 
 ;;; 
-;;; yasnippet-snippets provides a large amount of snippets for yasnippet to work with.
-;;; There are an innumerable amount of snippets for many, many languages.
-;;; This provides a good starting set of yasnippet snippets to use for most major modes.
+;; tempel-collection contains some ready-made templates.
 
-(use-package yasnippet-snippets
-  :after (yasnippet)
+(use-package tempel-collection
   :ensure t
-  :config (yasnippet-snippets-initialize))
+  :after (tempel))
 
 (provide 'snippets-config)
 ;;; snippet-config.el ends here
