6908d92c326b3927fa6279c38b83d0fc02a3434e
Karl Hallsby <karl@hallsby.com>
2026-02-09 23:34:40 -0600

    Ensure comint & comint-derived major-modes do not save duplicate input lines

--- config/comint-config.el
+++ config/comint-config.el
@@ -9,7 +9,10 @@
   ;; deleting part of the prompt, that is just confusing.
   (comint-prompt-read-only t)
   ;; Buffer's max size is 2KiB, default 1KiB is a bit small.
-  (comint-buffer-maximum-size (* 2 1024)))
+  (comint-buffer-maximum-size (* 2 1024))
+  ;; Make sure that comint (and anything that inherits from comint, I'm looking
+  ;; at you shell) ignored duplicated lines.
+  (comint-input-ignoredups t))
 
 (provide 'comint-config)
 ;;; comint-config.el ends here
