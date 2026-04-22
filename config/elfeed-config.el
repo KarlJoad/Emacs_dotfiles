eb890ebfbfd778d0d49009a307026c989ddfa716
Karl Hallsby <karl@hallsby.com>
2026-04-22 11:41:13 -0500

    Add Steve George (futurile)'s blog to elfeed

--- config/elfeed-config.el
+++ config/elfeed-config.el
@@ -58,6 +58,7 @@
                   ("https://griffinberlste.in/blog/atom.xml" hardware languages blog)
                   ("https://rachit.pl/post/atom.xml" hardware languages blog)
                   ("https://essenceia.github.io/index.xml" hardware asic blog)
+                  ("https://www.futurile.net/feeds/all.atom.xml" guix guile reproducible blog)
                   ("https://atillahallsby.com/feed/" blog rhetoric research)))
   ;; NOTE: Make elfeed use Emacs' built-in url-retrieve function rather than cURL.
   ;; This is significantly faster, but only works on Emacsen running on Linux
